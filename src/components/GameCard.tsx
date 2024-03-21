6b23664ca64384a5aa3baca12f732d72232e215e
Gaurav <gauravrawatgro99@gmail.com>
2024-03-21 11:39:25 +0530

    added platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
+import PlatformIconList from './PlatformIconList'
 
 
 function GameCard({game} : {game : Game}) {
@@ -8,6 +9,7 @@ function GameCard({game} : {game : Game}) {
         <Image src={game.background_image}/>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
+            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}></PlatformIconList>
         </CardBody>
     </Card>
   )
